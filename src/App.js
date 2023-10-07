c207df5b6f889c81ed93978d1ae7dc9a430caeb0
kavrad <kavyaht39@gmail.com>
2023-10-07 06:16:27 +0530

    Basic Set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import AddExpense from "./components/AddExpense";
+import ShowExpense from "./components/ShowExpenses";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen flex flex-col items-center gap-y-4 my-4">
+      <AddExpense></AddExpense>
+      <ShowExpense></ShowExpense>
     </div>
   );
 }
